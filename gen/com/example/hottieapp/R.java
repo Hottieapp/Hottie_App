f9c9f8408aa056496ee420a1bd6e9cd12e52735c
Roobal Jindal <roobaljindal9@gmail.com>
2014-09-09 15:52:17 +0530

    front design

--- gen/com/example/hottieapp/R.java
+++ gen/com/example/hottieapp/R.java
@@ -36,21 +36,23 @@ public final class R {
         public static final int ic_launcher=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000e;
-        public static final int boys=0x7f090009;
+        public static final int action_settings=0x7f090010;
+        public static final int boys=0x7f09000b;
         public static final int button=0x7f090004;
-        public static final int couples=0x7f090008;
-        public static final int fests=0x7f090007;
-        public static final int girls=0x7f090006;
+        public static final int button1=0x7f09000f;
+        public static final int couples=0x7f09000a;
+        public static final int fests=0x7f090008;
+        public static final int girls=0x7f090007;
         public static final int gridView=0x7f090000;
         public static final int imageView=0x7f090002;
         public static final int ratingBar1=0x7f090005;
+        public static final int rel=0x7f090006;
         public static final int textView=0x7f090003;
         public static final int textView1=0x7f090001;
-        public static final int textView2=0x7f09000a;
-        public static final int textView3=0x7f09000b;
-        public static final int textView4=0x7f09000c;
-        public static final int textView5=0x7f09000d;
+        public static final int textView2=0x7f090009;
+        public static final int textView3=0x7f09000c;
+        public static final int textView4=0x7f09000d;
+        public static final int textView5=0x7f09000e;
     }
     public static final class layout {
         public static final int activity_grid_contest_common=0x7f030000;
